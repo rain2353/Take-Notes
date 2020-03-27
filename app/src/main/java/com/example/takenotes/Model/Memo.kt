c417ae315808fbfb4b9b56c2a81ae39db289a0d2
rain2353 <vvkkvv@naver.com>
2020-03-27 18:02:40 +0900

    Model

---
+++ app/src/main/java/com/example/takenotes/Model/Memo.kt
@@ -0,0 +1,11 @@
+package com.example.takenotes.Model
+
+class Memo{
+    var num : Int? = null
+    var email : String? = null
+    var title : String? = null
+    var content : String? = null
+    var created_at : String? = null
+    var updated_at : String? = null
+
+}
---
+++ app/src/main/java/com/example/takenotes/Model/User.kt
@@ -0,0 +1,14 @@
+package com.example.takenotes.Model
+
+class User {
+    var num:Int? = null
+    var unique_id:String? = null
+    var email:String?=null
+    var name:String?=null
+    var nickname:String?=null
+    var phone_number:String?=null
+    var encrypted_password:String?=null
+    var salt: String? = null
+    var created_at: String? = null
+    var updated_at: String? = null
+}
