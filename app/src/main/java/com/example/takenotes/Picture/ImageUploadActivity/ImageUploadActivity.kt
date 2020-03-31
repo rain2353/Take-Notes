7a3b6999f22382759dda363028303742aca61136
rain2353 <vvkkvv@naver.com>
2020-03-31 19:16:18 +0900

    사용자가 이미지를 업로드하는 Activity

---
+++ app/src/main/java/com/example/takenotes/Picture/ImageUploadActivity/ImageUploadActivity.kt
@@ -0,0 +1,69 @@
+package com.example.takenotes.Picture.ImageUploadActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.takenotes.Common.Common
+import com.example.takenotes.R
+import kotlinx.android.synthetic.main.activity_image_upload.*
+
+class ImageUploadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_upload)
+        setToolbar()
+    }
+    // 툴바 사용 설정
+    private fun setToolbar() {
+        setSupportActionBar(ImageUploadToolbar)
+
+        // 툴바 왼쪽 버튼 설정
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp)  // 왼쪽 버튼 이미지 설정
+        supportActionBar!!.title = "사진 올리기"
+        supportActionBar!!.setDisplayShowTitleEnabled(true)    // 타이틀 안보이게 하기
+    }
+
+    // 툴바 메뉴 버튼을 설정
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.write_memo_save, menu)       // main_menu 메뉴를 toolbar 메뉴 버튼으로 설정
+        return true
+    }
+
+    // 툴바 버튼 설정
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        when (id) {
+            android.R.id.home -> {        // 뒤로가기 버튼
+                finish()
+                return true
+            }
+//            R.id.write_memo_save -> {     // 글 작성 완료 버튼
+//                if (editMemoTitle.text.isEmpty()) {
+//                    editMemoTitle.requestFocus()
+//                    Toast.makeText(this, "메모 제목을 입력해주세요.", Toast.LENGTH_SHORT).show()
+//                    return true
+//                } else if (editMemoContent.text.isEmpty()) {
+//                    editMemoContent.requestFocus()
+//                    Toast.makeText(this, "메모 내용을 입력해주세요.", Toast.LENGTH_SHORT).show()
+//                    return true
+//                } else {
+//                    WriteMemoUpload(
+//                        Common.UserInfomation?.email,
+//                        editMemoTitle.text.toString(),
+//                        editMemoContent.text.toString()
+//                    )
+//                }
+//                Log.d("memo_title", editMemoTitle.text.toString())
+//                Log.d("memo_content", editMemoContent.text.toString())
+//                Log.d("memo_writer", Common.UserInfomation?.email.toString())
+//            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
