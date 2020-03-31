25976a8a99f216e400c8afe9135b6826f42d6048
rain2353 <vvkkvv@naver.com>
2020-03-31 19:15:49 +0900

    Memo

--- app/src/main/java/com/example/takenotes/Memo/MemoFragment.kt
+++ app/src/main/java/com/example/takenotes/Memo/MemoFragment.kt
@@ -1,31 +1,37 @@
 package com.example.takenotes.Memo
 
-import android.content.Context
 import android.content.Intent
-import android.net.Uri
+import android.graphics.Canvas
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.takenotes.Api.INodeJS
 import com.example.takenotes.Api.RetrofitClient
 import com.example.takenotes.Common.Common
-import com.example.takenotes.Memo.Adapter.MemoRecyclerAdapter
+import com.example.takenotes.Controller.ItemTouchHelperCallback
 import com.example.takenotes.Memo.WriteMemoActivity.WriteMemoActivity
-
 import com.example.takenotes.R
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_memo.*
+import com.example.takenotes.Memo.Adapter.MemoRecyclerAdapter
+
+
 
 class MemoFragment : Fragment() {
 
     lateinit var myAPI: INodeJS
     var compositeDisposable = CompositeDisposable()
+    var helper: ItemTouchHelper? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,7 +57,11 @@ class MemoFragment : Fragment() {
         lm.reverseLayout = true   // 리사이클러뷰 역순 출력.
         lm.stackFromEnd = true    // 리사이클러뷰 역순 출력.
         MemoRecyclerView.layoutManager = lm
+
+
+
     }
+
 
     // 사용자의 이메일로 MySQL 에 저장된 DB를 확인한 후, 불러와서 사용자에게 리스트를 보여준다.
     private fun MemoList(email: String?) {
@@ -59,7 +69,16 @@ class MemoFragment : Fragment() {
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ Memo ->
-                MemoRecyclerView.adapter = MemoRecyclerAdapter(context!!, Memo)
+                //ItemTouchHelper 생성
+                val memoSwipeClickListener = MemoRecyclerAdapter(context!!, Memo)
+                helper = ItemTouchHelper( ItemTouchHelperCallback(memoSwipeClickListener))
+                helper!!.attachToRecyclerView(MemoRecyclerView)
+                MemoRecyclerView.addItemDecoration(object : RecyclerView.ItemDecoration(){
+                    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+                        helper!!.onDraw(c, parent, state)
+                    }
+                })
+                MemoRecyclerView.adapter = memoSwipeClickListener
                 (MemoRecyclerView.adapter as MemoRecyclerAdapter).notifyDataSetChanged()
             }
                 , { thr ->
@@ -69,8 +88,12 @@ class MemoFragment : Fragment() {
             ))
     }
 
-    override fun onResume() {    // 메모를 작성하고 나왔을때 작성한 메모가 리스트 맨위에서 볼수 있도록 한다.
+    override fun onStart() {    // 메모를 작성하고 나왔을때 작성한 메모가 리스트 맨위에서 볼수 있도록 한다.
         MemoList(Common.UserInfomation?.email)
-        super.onResume()
+        super.onStart()
     }
+
+
 }
+
+
