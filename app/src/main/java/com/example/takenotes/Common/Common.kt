19e0de4b724a58bba8ed1950a8fa5481fc86833c
rain2353 <vvkkvv@naver.com>
2020-04-06 22:21:34 +0900

    Retrofit2 통신

--- app/src/main/java/com/example/takenotes/Common/Common.kt
+++ app/src/main/java/com/example/takenotes/Common/Common.kt
@@ -2,12 +2,14 @@ package com.example.takenotes.Common
 
 import com.example.takenotes.Api.INodeJS
 import com.example.takenotes.Api.RetrofitClient
+import com.example.takenotes.Model.Image
 import com.example.takenotes.Model.Memo
 import com.example.takenotes.Model.User
 
 object Common {
-        var UserInfomation : User? = null    // 로그인한 유저의 정보를 가지고 있는다.
-        var SelectMemo : Memo? = null        // 사용자가 작성된 메모 리사이클러뷰 아이템을 선택하면 그 아이템에 대한 정보들을 가지고 있는다.
+    var UserInfomation : User? = null    // 로그인한 유저의 정보를 가지고 있는다.
+    var SelectMemo : Memo? = null        // 사용자가 작성된 메모 리사이클러뷰 아이템을 선택하면 그 아이템에 대한 정보들을 가지고 있는다.
+    var SelectPicture : Image? = null   // 사용자가 업로드한 사진 리스트중 선택한 아이템의 정보를 가지고 있다.
 
 
 
