1d040012a78ac54662f4c4d82952921b4410834c
rain2353 <vvkkvv@naver.com>
2020-04-06 22:21:16 +0900

    Retrofit2 통신

--- app/src/main/java/com/example/takenotes/Api/INodeJS.kt
+++ app/src/main/java/com/example/takenotes/Api/INodeJS.kt
@@ -1,8 +1,16 @@
 package com.example.takenotes.Api
 
 
+import androidx.test.runner.permission.RequestPermissionCallable
+import com.example.takenotes.Model.Image
 import com.example.takenotes.Model.Memo
+import io.reactivex.Flowable
 import io.reactivex.Observable
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.adapter.rxjava2.Result
 import retrofit2.http.*
 
 interface INodeJS {
@@ -57,4 +65,26 @@ interface INodeJS {
     @POST("DeleteMemo")
     @FormUrlEncoded
     fun DeleteMemo(@Field("num") num: Int?): Observable<String>
+
+    // 사용자가 선택한 이미지 업로드
+
+    @POST("ImageUpload")
+    @Multipart
+    fun ImageUpload(
+        @Part files : ArrayList<MultipartBody.Part>,
+        @Part ("email") email: String?,
+        @Part ("title") title: String?,
+        @Part ("content") content: String?
+    ) : Call<String>
+
+    // 업로드한 사진 리스트 불러오기
+    @GET("ImageList/{email}")
+    fun ImageList(
+        @Path("email") email: String?
+    ): Observable<List<Image>>
+
+    // 사진 삭제
+    @POST("DeletePicture")
+    @FormUrlEncoded
+    fun DeletePicture(@Field("num") num: Int?): Observable<String>
 }
