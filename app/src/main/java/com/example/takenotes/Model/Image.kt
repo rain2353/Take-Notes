c446dadad9690726041d97369a265deb76aba68a
rain2353 <vvkkvv@naver.com>
2020-04-06 22:22:58 +0900

    Image model

---
+++ app/src/main/java/com/example/takenotes/Model/Image.kt
@@ -0,0 +1,20 @@
+package com.example.takenotes.Model
+
+class Image {
+    var num : Int? = null
+    var email : String? = null
+    var title : String? = null
+    var content : String? = null
+    var file : String? = null
+    var file1 : String? = null
+    var file2 : String? = null
+    var file3 : String? = null
+    var file4 : String? = null
+    var file5 : String? = null
+    var file6 : String? = null
+    var file7 : String? = null
+    var file8 : String? = null
+    var file9 : String? = null
+    var created_at : String? = null
+    var updated_at : String? = null
+}
