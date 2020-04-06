4ef2d7d53c69dbae8e9fe6ea8a44764d0b687167
rain2353 <vvkkvv@naver.com>
2020-04-06 22:25:19 +0900

    View Pager Fragment

---
+++ app/src/main/java/com/example/takenotes/PictureViewPager/Fragment/ViewPagerFragment4.kt
@@ -0,0 +1,26 @@
+package com.example.takenotes.PictureViewPager.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.takenotes.Common.Common
+import com.example.takenotes.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.view_pager_fragment.*
+
+class ViewPagerFragment4: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.view_pager_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        //뷰 설정
+        Picasso.get().load("http://10.0.2.2:3000/"+ Common.SelectPicture?.file4).into(PictureView)
+
+    }
+}
