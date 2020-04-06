37561b1e568f1601d4f4fe235ad148e97835f5db
rain2353 <vvkkvv@naver.com>
2020-04-06 22:24:27 +0900

    Picture

--- app/src/main/java/com/example/takenotes/Picture/PictureFragment.kt
+++ app/src/main/java/com/example/takenotes/Picture/PictureFragment.kt
@@ -2,23 +2,42 @@ package com.example.takenotes.Picture
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Canvas
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.takenotes.Api.INodeJS
+import com.example.takenotes.Api.RetrofitClient
+import com.example.takenotes.Common.Common
+import com.example.takenotes.Controller.ItemTouchHelperCallback
+import com.example.takenotes.Picture.Adapter.PictureRecyclerAdapter
 import com.example.takenotes.Picture.ImageUploadActivity.ImageUploadActivity
 
 import com.example.takenotes.R
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_picture.*
 
 class PictureFragment : Fragment() {
-
+    lateinit var myAPI: INodeJS
+    var compositeDisposable = CompositeDisposable()
+    var helper: ItemTouchHelper? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //Init API
+        val retrofit = RetrofitClient.instance
+        myAPI = retrofit.create(INodeJS::class.java)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_picture, container, false)
     }
@@ -29,6 +48,41 @@ class PictureFragment : Fragment() {
             val intent = Intent(context, ImageUploadActivity::class.java)
             startActivity(intent)
         }
+        // 리사이클러뷰 레이아웃매니저를 설정한다.
+        val lm = LinearLayoutManager(context)
+        lm.reverseLayout = true   // 리사이클러뷰 역순 출력.
+        lm.stackFromEnd = true    // 리사이클러뷰 역순 출력.
+        PictureRecyclerView.layoutManager = lm
+
         super.onViewCreated(view, savedInstanceState)
+    }
+    // 사용자의 이메일로 MySQL 에 저장된 DB를 확인한 후, 불러와서 사용자에게 리스트를 보여준다.
+    private fun ImageList(email: String?) {
+        compositeDisposable.add(myAPI.ImageList(email)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ Image ->
+                //ItemTouchHelper 생성
+                val pictureSwipeClickListener = PictureRecyclerAdapter(context!!, Image)
+                helper = ItemTouchHelper( ItemTouchHelperCallback(pictureSwipeClickListener))
+                helper!!.attachToRecyclerView(PictureRecyclerView)
+                PictureRecyclerView.addItemDecoration(object : RecyclerView.ItemDecoration(){
+                    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+                        helper!!.onDraw(c, parent, state)
+                    }
+                })
+                PictureRecyclerView.adapter = pictureSwipeClickListener
+                (PictureRecyclerView.adapter as PictureRecyclerAdapter).notifyDataSetChanged()
+            }
+                , { thr ->
+                    Log.d("MemoRecyclerView", thr.message.toString())
+                }
+
+            ))
+    }
+
+    override fun onStart() {    // 메모를 작성하고 나왔을때 작성한 메모가 리스트 맨위에서 볼수 있도록 한다.
+        ImageList(Common.UserInfomation?.email)
+        super.onStart()
     }
 }
