f474f53e9b4e8471d0d1a25eb2ac00a3ae1fe76f
rain2353 <vvkkvv@naver.com>
2020-04-07 22:54:49 +0900

    Video

--- app/src/main/java/com/example/takenotes/Video/VideoFragment.kt
+++ app/src/main/java/com/example/takenotes/Video/VideoFragment.kt
@@ -1,6 +1,7 @@
 package com.example.takenotes.Video
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,6 +10,8 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.takenotes.R
+import com.example.takenotes.Video.VideoUploadActivity.VideoUploadActivity
+import kotlinx.android.synthetic.main.fragment_video.*
 
 
 class VideoFragment : Fragment() {
@@ -21,6 +24,15 @@ class VideoFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_video, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // 화면에 보이는 플러스 모양의 FloatingActionButton 을 누르면 동영상을 업로드할수 있는 액티비티로 이동한다.
+        this.VideoUploadButton.setOnClickListener {
+            val intent = Intent(context, VideoUploadActivity::class.java)
+            startActivity(intent)
+        }
 
+
+        super.onViewCreated(view, savedInstanceState)
+    }
 
 }
