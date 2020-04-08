5cb54858b61b76c79d01677c3352663a8dae7d08
rain2353 <vvkkvv@naver.com>
2020-04-08 18:12:38 +0900

    Model

---
+++ app/src/main/java/com/example/takenotes/Model/Video.kt
@@ -0,0 +1,13 @@
+package com.example.takenotes.Model
+
+class Video {
+
+    var num: Int? = null
+    var email: String? = null
+    var title: String? = null
+    var content: String? = null
+    var video: String? = null
+    var created_at: String? = null
+    var updated_at: String? = null
+
+}
