f89bd161319f505b5a1ab17c145f99cebc04025e
rain2353 <vvkkvv@naver.com>
2020-04-08 18:15:24 +0900

    Video Play Activity

---
+++ app/src/main/java/com/example/takenotes/Video/VideoPlayActivity/VideoPlayActivity.kt
@@ -0,0 +1,13 @@
+package com.example.takenotes.Video.VideoPlayActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.takenotes.R
+
+class VideoPlayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_play)
+    }
+}
