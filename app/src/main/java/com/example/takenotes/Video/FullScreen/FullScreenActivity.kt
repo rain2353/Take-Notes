eb30df1632e105dee277bf18ceb24c0a9927df12
rain2353 <vvkkvv@naver.com>
2020-04-09 22:44:04 +0900

    Video Full Screen Activity

---
+++ app/src/main/java/com/example/takenotes/Video/FullScreen/FullScreenActivity.kt
@@ -0,0 +1,45 @@
+package com.example.takenotes.Video.FullScreen
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.takenotes.R
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import kotlinx.android.synthetic.main.activity_full_screen.*
+
+
+class FullScreenActivity : AppCompatActivity() {
+
+
+    private var player: SimpleExoPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_screen)
+
+        player = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
+        playerView.setPlayer(player)
+
+        val intent = intent
+        val videoUrl = intent.getStringExtra("videoUrl")
+        val currentPos = intent.getLongExtra("currentPos", 0)
+
+
+        //미디어 소스 객체 생성
+        val factory: DataSource.Factory = DefaultDataSourceFactory(this, "Ex90Exoplayer")
+        val mediaFactory = ProgressiveMediaSource.Factory(factory)
+
+        val mediaSource =
+            mediaFactory.createMediaSource(Uri.parse(videoUrl))
+        player!!.prepare(mediaSource)
+        player!!.setPlayWhenReady(true)
+
+        player!!.seekTo(currentPos)
+    }
+}
